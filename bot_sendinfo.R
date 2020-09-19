bb2a1af7ffd1981fd7db73fc91e060f1bb77d469
Saulogr <saulog46@gmail.com>
2020-09-19 17:32:55 -0300

    Criação da estrutura de resposta

--- bot_sendinfo.R
+++ bot_sendinfo.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(telegram)
 library(lubridate)
+library(jpeg)
 
 # Configrando o BOT
 bot = TGBot$new(token = '1373216588:AAESixBPt6Pz1duaWgztF3lKgL2BCkS0l_c' )
@@ -26,14 +27,29 @@ if (ultimo_update == atual_update){
 } else {
   
   print("Houve mudança")
+
+  # Salvando as informações necessárias no data frame
+  df_info = data.frame(id_update = atlz$update_id, user = atlz[[2]][3][,1], atlz[[2]][4], atlz[[2]][5] )[, c(-4)]
+  df_info$date = as_datetime(df_info$date, tz = "America/Maceio")
+  mensagem = df_info %>% filter(id_update == ultimo_update +1) %>%
+    select(text) %>% as.character()
+  
+  user.id = df_info %>% filter(id_update == ultimo_update +1) %>%
+    select(user.id) %>% as.integer()
+  
+  if (mensagem == "Olá"){
+    
+    img = readJPEG("CJNYCB45H4GE56DH5YB3AV7BX4.jpg" )
+    
+    bot$set_default_chat_id(user.id)
+    bot$sendPhoto("CJNYCB45H4GE56DH5YB3AV7BX4.jpg", caption = 'Aqui tem coragem')
+  }
+  
+  
+  # Salvando em um arquivo auxiliar
   write.csv(atual_update, "lastupdate.tlb" )
 }
 
 }
 
-# Salvando em um arquivo auxiliar
 
-# Salvando as informações neessárias no data frame
-df_info = data.frame(atlz[[2]][3][,1], atlz[[2]][4], atlz[[2]][5] )[, c(-4)]
-# Mudando o formato da data
-df_info$date = as_datetime(df_info$date, tz = "America/Maceio")
